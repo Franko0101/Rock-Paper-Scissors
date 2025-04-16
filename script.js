7cb7b78b8866c3187a698fffc1bee5f095ac72b0
Franko0101 <163135894+Franko0101@users.noreply.github.com>
2025-04-16 16:57:01 +0200

    Playable round.
    Implemented function playRound that gets human
    and computer choice, does the comparison,
    print who won and increment the correct score.

--- script.js
+++ script.js
@@ -15,3 +15,69 @@ function getComputerChoice() {
 function getHumanChoice() {
     return prompt("Insert rock, paper or scissors");
 }
+
+let humanScore = 0, computerScore = 0;
+
+function printResult(humanChoice, computerChoice, result) {
+    console.log(
+        `
+        Human: ${humanChoice}
+        Computer: ${computerChoice}
+        Result: ${result}
+        `
+    );
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+    if (humanChoice == computerChoice) {
+        printResult(humanChoice, computerChoice, "Thats a draw!")
+        return;
+    } else {
+        switch (humanChoice) {
+            case "rock":
+                switch(computerChoice) {
+                    case "paper":
+                        printResult(humanChoice, computerChoice, "Computer Wins!");
+                        computerScore++;
+                        return;
+                
+                    case "scissors":
+                        printResult(humanChoice, computerChoice, "Player Wins!");
+                        humanScore++;
+                        return;
+            }
+
+            case "paper":
+                switch(computerChoice) {
+                    case "scissors":
+                        printResult(humanChoice, computerChoice, "Computer Wins!");
+                        computerScore++;
+                        return;
+                
+                    case "rock":
+                        printResult(humanChoice, computerChoice, "Player Wins!");
+                        humanScore++;
+                        return;
+            }
+
+            default:
+                switch(computerChoice) {
+                    case "rock":
+                        printResult(humanChoice, computerChoice, "Computer Wins!");
+                        computerScore++;
+                        return;
+                
+                    case "paper":
+                        printResult(humanChoice, computerChoice, "Player Wins!");
+                        humanScore++;
+                        return;
+            }
+        }
+    }
+}
+
+let humanSelection = getHumanChoice(), computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
