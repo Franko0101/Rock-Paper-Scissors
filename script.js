b725200109890980ee4bd919bd560129e467658f
Franko0101 <163135894+Franko0101@users.noreply.github.com>
2025-04-19 17:33:45 +0200

    Implemented UI.

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+const choiceDiv = document.createElement("div");
+
+const choicePara = document.createElement("p");
+choicePara.textContent = "Click a button to make your choice";
+
+const rock = document.createElement("button");
+rock.textContent = "rock";
+
+const paper = document.createElement("button");
+paper.textContent = "paper";
+
+const scissors = document.createElement("button");
+scissors.textContent = "scissors";
+
+choiceDiv.appendChild(choicePara);
+choiceDiv.appendChild(rock);
+choiceDiv.appendChild(paper);
+choiceDiv.appendChild(scissors);
+document.body.appendChild(choiceDiv);
+
+const gameDiv = document.createElement("div");
+
+const scorePara = document.createElement("p");
+
+document.body.appendChild(gameDiv);
+
+let humanScore = 0, computerScore = 0;
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     switch(randomNumber) {
@@ -12,97 +40,91 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    return prompt("Insert rock, paper or scissors");
-}
-
 function printResult(humanChoice, computerChoice, result) {
-    console.log(
-        `
-        Human: ${humanChoice}
-        Computer: ${computerChoice}
-        Result: ${result}
-        `
-    );
+    const resultPara = document.createElement("p");
+
+    resultPara.textContent = 
+    `Human: ${humanChoice}
+     Computer: ${computerChoice}
+     Result: ${result}`;
+    
+    gameDiv.appendChild(resultPara);
+    scorePara.textContent = `Human Score: ${humanScore} \t Computer Score: ${computerScore}`
+    gameDiv.appendChild(scorePara);
 }
 
-function playGame() {
+function endGame() {
 
-    let humanSelection = getHumanChoice(), computerSelection = getComputerChoice();
-    let humanScore = 0, computerScore = 0;
+    if (humanScore == 5) {
+        const endPara = document.createElement("p");
+        endPara.textContent = "You win!";
+        gameDiv.appendChild(endPara);
+    } else if (computerScore == 5) {
+        const endPara = document.createElement("p");
+        endPara.textContent = "Computer wins!";
+        gameDiv.appendChild(endPara);
+    }
+}
 
-    function playRound(humanChoice, computerChoice) {
+function playRound(e) {
 
-        humanChoice = humanChoice.toLowerCase();
-        computerChoice = computerChoice.toLowerCase();
+    let humanChoice = e.target.textContent, computerChoice = getComputerChoice();
 
-        if (humanChoice == computerChoice) {
-            printResult(humanChoice, computerChoice, "Thats a draw!")
-            return;
-        } else {
-            switch (humanChoice) {
-                case "rock":
-                    switch(computerChoice) {
-                        case "paper":
-                            printResult(humanChoice, computerChoice, "Computer wins!");
-                            computerScore++;
-                            return;
-                    
-                        case "scissors":
-                            printResult(humanChoice, computerChoice, "You win!");
-                            humanScore++;
-                            return;
-                }
-    
-                case "paper":
-                    switch(computerChoice) {
-                        case "scissors":
-                            printResult(humanChoice, computerChoice, "Computer wins!");
-                            computerScore++;
-                            return;
-                    
-                        case "rock":
-                            printResult(humanChoice, computerChoice, "You win!");
-                            humanScore++;
-                            return;
-                }
-    
-                default:
-                    switch(computerChoice) {
-                        case "rock":
-                            printResult(humanChoice, computerChoice, "Computer wins!");
-                            computerScore++;
-                            return;
-                    
-                        case "paper":
-                            printResult(humanChoice, computerChoice, "You win!");
-                            humanScore++;
-                            return;
-                }
+    if (humanChoice == computerChoice) {
+        printResult(humanChoice, computerChoice, "Thats a draw!")
+        return;
+    } else {
+        switch (humanChoice) {
+            case "rock":
+                switch(computerChoice) {
+                    case "paper":
+                        computerScore++;
+                        printResult(humanChoice, computerChoice, "Computer wins!");
+                        endGame();
+                        return;
+                
+                    case "scissors":
+                        humanScore++;
+                        printResult(humanChoice, computerChoice, "You win!");
+                        endGame();
+                        return;
+            }
+
+            case "paper":
+                switch(computerChoice) {
+                    case "scissors":
+                        computerScore++;
+                        printResult(humanChoice, computerChoice, "Computer wins!");
+                        endGame();
+                        return;
+                
+                    case "rock":
+                        humanScore++;
+                        printResult(humanChoice, computerChoice, "You win!");
+                        endGame();
+                        return;
+            }
+
+            default:
+                switch(computerChoice) {
+                    case "rock":
+                        computerScore++;
+                        printResult(humanChoice, computerChoice, "Computer wins!");
+                        endGame();
+                        return;
+                
+                    case "paper":
+                        humanScore++;
+                        printResult(humanChoice, computerChoice, "You win!");
+                        endGame();
+                        return;
             }
         }
     }
-
-    for (i = 0; i<5; i++) {
-        playRound(humanSelection, computerSelection);
-        humanSelection = getHumanChoice();
-        computerSelection = getComputerChoice();
-    }
-
-    console.log(
-        `
-        Human Score: ${humanScore}
-        Computer Score: ${computerScore}
-        `
-    );
-
-    if(humanScore == computerScore) {
-        console.log("That's a draw!");
-    } else if (humanScore > computerScore) {
-        console.log("You Win!");
-    } else {
-        console.log("Computer Win!");
-    }
 }
 
-playGame();
+rock.addEventListener("click", playRound);
+paper.addEventListener("click", playRound);
+scissors.addEventListener("click", playRound);
+
+//playGame();
